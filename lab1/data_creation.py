f65b7896681512df4d47496aefd5086f46bf8525
Alex Izmalkin <sndbox44@gmail.com>
2024-04-08 11:42:21 -0300

    modify data_creation.py and pipeline.sh

--- lab1/data_creation.py
+++ lab1/data_creation.py
@@ -1,5 +1,6 @@
 import numpy as np  # to work with arrays
 import os  # to work with directories and files
+import sys  # to work with command line arguments
 # to create polynomial features
 from sklearn.preprocessing import PolynomialFeatures
 # to create a linear regression model
@@ -12,7 +13,8 @@ from sklearn.model_selection import train_test_split
 
 # create constant values for the data size,
 # polynomial degree, noise, and number of anomalies
-data_size, poly_degree, noise, anomalies = 500, 4, 3, 0
+data_size, poly_degree = int(sys.argv[1]), int(sys.argv[2])
+noise, anomalies = int(sys.argv[3]), int(sys.argv[4])
 # generate random data
 x_data = np.arange(1, data_size + 1).reshape(-1, 1)
 y = np.random.uniform(0, 200, (data_size, ))
--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-
-python3 ./data_creation.py
+# This script is used to create the data for the pipeline
+DATA_SIZE=$1
+POLY_DEGREE=$2
+NOISE=$3
+ANOMALIES=$4
+python3 ./data_creation.py $DATA_SIZE $POLY_DEGREE $NOISE $ANOMALIES
